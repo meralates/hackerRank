bb1001de7220fd9f231ebfa709306a8e13c79a85
meralates <202103001075@dogus.edu.tr>
2024-10-25 14:17:15 +0300

    İlk commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
-
 class Player {
     String name;
     int score;
@@ -11,39 +10,37 @@ class Player {
         this.score = score;
     }
 }
-
 class Checker implements Comparator<Player> {
     @Override
     public int compare(Player a, Player b) {
         if (b.score != a.score) {
-            return b.score - a.score; // Yüksek puandan düşük puana sıralama
+            return b.score - a.score;
         } else {
-            return a.name.compareTo(b.name); // İsim sıralaması
+            return a.name.compareTo(b.name);
         }
     }
 }
-
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt(); // Oyuncu sayısını al
-        scanner.nextLine(); // Satır sonunu atla
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
         Player[] players = new Player[n];
         for (int i = 0; i < n; i++) {
-            String name = scanner.nextLine(); // İsim girişi
-            int score = scanner.nextInt(); // Puan girişi
-            scanner.nextLine(); // Bir sonraki satır sonunu atla
-            players[i] = new Player(name, score); // Oyuncuyu oluştur
+            String name = scanner.nextLine();
+            int score = scanner.nextInt();
+            scanner.nextLine();
+            players[i] = new Player(name, score);
         }
 
-        Arrays.sort(players, new Checker()); // Oyuncuları sıralama
+        Arrays.sort(players, new Checker());
 
         for (Player player : players) {
-            System.out.println(player.name + " " + player.score); // Sıralı çıktı
+            System.out.println(player.name + " " + player.score);
         }
 
-        scanner.close(); // Scanner'ı kapat
+        scanner.close();
     }
 }
